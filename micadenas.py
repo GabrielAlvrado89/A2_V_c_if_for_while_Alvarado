33d0859aab4b1ff26961b66577e7322f7b447287
Gabriel Alvarado <a.22308051281127@cbtis128.edu.mx>
2024-09-06 09:33:20 -0600

    Add files via upload
    Subí dos archivos

---
+++ micadenas.py
@@ -0,0 +1,27 @@
+'''Angel Alvarado,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+
+nombre="Angel"
+print(nombre[0])
+print(nombre)
+for c in "Angel Alvarado ": #e
+ print(c)
+MiNovias=["Michelle,Miranda,Andrea Guzman"]
+for chicas in MiNovias :
+ print(chicas)
+
+
+
+ print("otro ejemplo")
+ b = "Angel, Alvarado"
+print(b[2:5])
+print(" a mayusculas")
+a = "angel alvarado"
+print(a.upper())
+print("Concatenar")
+a = "Angel"
+b = "Alvarado"
+c = a +"_"+ b
+print(c)
---
+++ mivariables.py
@@ -0,0 +1,15 @@
+import math
+edad=17 # 17 se guarda  en edad
+nombre= "Jesus Adrian" # cadena
+print("tu edad es ", edad)
+print("te llamas", nombre)
+print("-- tipo de variables --")
+x = str(7)    # x will be '3'
+y = int(3.141592)    # y will be 3
+z = float(8)
+print(x)
+print(y)
+print(z)
+print(" -- Funciones Matematicas --")
+x = math.sqrt(64) # Funcion para sacar la raiz y guarda el resultado en x
+print(x)
