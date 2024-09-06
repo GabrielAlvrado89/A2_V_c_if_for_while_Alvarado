4a6d18c8556a8c53eb359b5403ba460b41786438
Gabriel Alvarado <a.22308051281127@cbtis128.edu.mx>
2024-09-06 10:15:50 -0600

    Add files via upload

---
+++ ejemplofor.py
@@ -0,0 +1,3 @@
+fruits = ["manzana", "platano", "fresa"] # Contenido de la variable
+for x in fruits: # Funciona como un contador
+  print(x)  # imprime en pantalla el contenido uno en uno
---
+++ ejemplowhile.py
@@ -0,0 +1,6 @@
+i = 1 # Se le asigna el valor de 1 
+while i < 6: # Determina si es falso o verdadero
+  print(i) # Imprime el contenido
+  i += 1 # hace una suma sucesiva
+else:
+  print("i is no longer less than 6")
